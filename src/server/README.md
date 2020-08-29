1ea620239e5a0005ec1d0f2a2de12c3231431576
Zach Wild <zacharyjwild@gmail.com>
2020-08-29 00:36:35 -0400

    Update npm packages

---
+++ src/server/README.md
@@ -0,0 +1,6 @@
+## Entities
+
+
+## Requests
+
+## Responses
--- src/server/game.js
+++ src/server/game.js
@@ -9,7 +9,7 @@ class Game {
     this.bullets = [];
     this.lastUpdateTime = Date.now();
     this.shouldSendUpdate = false;
-    setInterval(this.update.bind(this), 1000 / 60);
+    setInterval(this.update.bind(this), 1000 / 60); //
   }
 
   addPlayer(socket, username) {
