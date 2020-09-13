fde2ed1dac815628665b60a7e2284c1901e8c911
Zach Wild <zacharyjwild@gmail.com>
2020-09-13 02:37:44 -0400

    done with collisions

--- src/server/bullet.js
+++ src/server/bullet.js
@@ -8,16 +8,11 @@ class Bullet extends ObjectClass {
     const hitbox = new Shape([[0, 0], [3, 0], [3, 3], [0, 3]], x, y);
     super(shortid(), x, y, dir, Constants.BULLET_SPEED, hitbox);
     this.parentID = parentID;
-
-    // need to get the length and width from the ship, but for now we do this shit
-    this.box = new Shape([[0, 0], [3, 0], [3, 3], [0, 3]], x, y);
   }
 
   // Returns true if the bullet should be destroyed
   update(dt) {
     super.update(dt);
-    this.box.x = this.x;
-    this.box.y = this.y;
     return this.x < 0 || this.x > Constants.MAP_SIZE || this.y < 0 || this.y > Constants.MAP_SIZE;
   }
 }
