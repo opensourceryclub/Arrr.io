9b4924f6593d8cf6f448e61a8c10e17a956659cd
Zach Wild <zacharyjwild@gmail.com>
2020-10-15 23:21:38 -0400

    Add some comments for the steering systems

--- src/server/systems/steering.js
+++ src/server/systems/steering.js
@@ -1,5 +1,15 @@
 const { rotateBy } = require('../utils/utils');
 
+/**
+ * Update a ship's rudder angle. Player's can use the a,d keys to turn their rudder
+ * to the left or right. This method will increase or decrease the rudder angle depending
+ * on which keys are pressed. It will also ensure that the rudder angle stays within the
+ * bounds of the ship's rudder. Each ship has a unique min,max rudder angle and speed for
+ * how quickly the rudder can turn.
+ *
+ * @param {*} player
+ * @param {*} directions
+ */
 function updateSteering(player, directions) {
   // Update the ship's rudder angle, turn it left or right depending on input and the ships steering speed
   player.rudderAngle = rotateBy(player.rudderAngle, (-directions[0] + directions[1]) * player.rudderSpeed);
@@ -8,7 +18,10 @@ function updateSteering(player, directions) {
 }
 
 /**
- * Update a player's ship's sails.
+ * Update a player's ship's sails. Whether or not a ship's sails are raised will determine
+ * if its velocity will increase or begin decreasing. Players use the w,d keys to raise,lower
+ * their sails. The velocity effects take place in the `updatePlayerPositions` system of
+ * the game loop.
  *
  * @param {*} player
  * @param {*} input
